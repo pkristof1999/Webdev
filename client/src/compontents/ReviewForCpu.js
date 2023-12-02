a5d4a9ed786226b86cd776fd97d2a876df63c56d
Kristóf Géza Pankotai <p.kristof1999@gmail.com>
2023-12-02 22:21:51 +0100

    Frontend javítása.

--- client/src/compontents/ReviewForCpu.js
+++ client/src/compontents/ReviewForCpu.js
@@ -1,5 +1,29 @@
-const ReviewForCpu = {
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ApiService from '../services/ApiService';
 
-}
+const ReviewForCpu = () => {
+	const { cpuID } = useParams();
+	const [reviews, setReviews] = useState([]);
+
+	useEffect(() => {
+		ApiService.getReviews(cpuID)
+			.then((response) => setReviews(response.data))
+			.catch((error) => console.error('Error fetching reviews:', error));
+	}, [cpuID]);
+
+	return (
+		<div>
+			<h2>Reviews for CPU {cpuID}</h2>
+			<ul>
+				{reviews.map((review) => (
+					<li key={review.id}>
+						Reviewer: {review.reviewer}, Score: {review.score}, Recommend: {review.recommend ? 'Yes' : 'No'}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default ReviewForCpu;
--- client/src/compontents/ReviewForCpuUpload.js
+++ client/src/compontents/ReviewForCpuUpload.js
@@ -1,5 +1,40 @@
-const ReviewForCpuUpload = {
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../services/ApiService';
 
-}
+const ReviewForCpuUpload = ({ cpuId }) => {
+	const [reviewData, setReviewData] = useState({
+		reviewer: '',
+		reviewText: '',
+		score: 0,
+		recommend: false,
+	});
+
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		setReviewData({ ...reviewData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			await ApiService.createReview(cpuId, reviewData);
+			navigate(`/ReviewForCpu/${cpuId}`);
+		} catch (error) {
+			console.error('Error creating review:', error);
+		}
+	};
+
+	return (
+		<div>
+			<h2>Add Review</h2>
+			<form onSubmit={handleSubmit}>
+				{/* Review form fields */}
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+};
 
 export default ReviewForCpuUpload;
