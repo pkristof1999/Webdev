4f0143895b4f2a77a036d60ef08313de8bd681d2
Kristóf Géza Pankotai <p.kristof1999@gmail.com>
2023-12-02 20:06:41 +0100

    Stílusok hozzáadása.

--- client/src/compontents/CpuList.js
+++ client/src/compontents/CpuList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ApiService from '../services/ApiService';
 
 const CpuList = () => {
+    const navigate = useNavigate();
     const [cpus, setCPUs] = useState([]);
 
     useEffect( () => {
@@ -11,38 +12,54 @@ const CpuList = () => {
             .catch((error) => console.error('Error fetching CPUs:', error));
     }, []);
 
+    const navigateCpuUpload = () => {
+        navigate("/CpuUpload");
+    }
+
     console.log(cpus);
 
     return (
         <div className={"main-container"}>
-            <h2>CPU List</h2>
-            <table>
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Gyártó</th>
-                    <th>Model</th>
-                    <th>Frekvencia</th>
-                    <th>Magok száma</th>
-                    <th>Értékelések</th>
-                    <th>Törlés</th>
-                </tr>
-                </thead>
-                <tbody>
-                {cpus.map((cpu) => (
-                    <tr key={cpu.id}>
-                        <td>{cpu.id}</td>
-                        <td>{cpu.manufacturer}</td>
-                        <td>{cpu.model}</td>
-                        <td>{cpu.frequency}</td>
-                        <td>{cpu.coreCount}</td>
-                        <td><Link to={`/ReviewForCpu/${cpu.id}`}>Értékelések</Link></td>
-                        <td><button>Delete</button></td>
+            <h2>Processzorok listája</h2>
+            <div className={"board-blur-container"}/>
+            <div className={"board-container"}>
+                <table>
+                    <thead className={"thead-style"}>
+                    <tr>
+                        <th>ID</th>
+                        <th>Gyártó</th>
+                        <th>Model</th>
+                        <th>Frekvencia</th>
+                        <th>Magok száma</th>
+                        <th>Értékelések</th>
+                        <th>Törlés</th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
-            <Link to="/CpuUpload">Add CPU</Link>
+                    </thead>
+                    <tbody>
+                    {cpus.map((cpu) => (
+                        <tr key={cpu.id}>
+                            <td>{cpu.id}</td>
+                            <td>{cpu.manufacturer}</td>
+                            <td>{cpu.model}</td>
+                            <td>{cpu.frequency}</td>
+                            <td>{cpu.coreCount}</td>
+                            <td>
+                                <Link className={"table-button-style link"} to={`/ReviewForCpu/${cpu.id}`}>Értékelések</Link>
+                            </td>
+                            <td>
+                                <button className={"table-button-style"}>
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                <button className={"button-style"}
+                        onClick={navigateCpuUpload}>
+                    Processzor Hozzáadása
+                </button>
+            </div>
         </div>
     );
 };
--- client/src/compontents/CpuUpload.js
+++ client/src/compontents/CpuUpload.js
@@ -1,5 +1,36 @@
-const CpuUpload = {
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../services/ApiService';
 
-}
+const CpuUpload = () => {
+	const [cpuData, setCPUData] = useState({
+		manufacturer: '',
+		model: '',
+		frequency: '',
+		coreCount: 0,
+	});
+
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		setCPUData({ ...cpuData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		ApiService.createCPUEntry(cpuData)
+			.then(() => navigate('/CPUList'))
+			.catch((error) => console.error('Error creating CPU:', error));
+	};
+
+	return (
+		<div>
+			<h2>Add CPU</h2>
+			<form onSubmit={handleSubmit}>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+};
 
 export default CpuUpload;
