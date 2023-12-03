3f90691fd7cb617f447a63cc9cd8ccca91b7c866
Kristóf Géza Pankotai <p.kristof1999@gmail.com>
2023-12-03 12:43:22 +0100

    Néhány alap értékelés hozzáadása.

--- server/src/main/java/hu/unideb/inf/config/LoadInitialData.java
+++ server/src/main/java/hu/unideb/inf/config/LoadInitialData.java
@@ -1,20 +1,26 @@
 package hu.unideb.inf.config;
 
+import hu.unideb.inf.service.ReviewService;
 import hu.unideb.inf.uito.CPUModelUITO;
+import hu.unideb.inf.uito.ReviewOfCPUUITO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import hu.unideb.inf.service.CPUService;
 
+import java.util.List;
+
 @Component
 public class LoadInitialData implements CommandLineRunner {
 
     private final CPUService cpuService;
+    private final ReviewService reviewService;
 
     @Autowired
-    public LoadInitialData(CPUService cpuService) {
+    public LoadInitialData(CPUService cpuService, ReviewService reviewService) {
         this.cpuService = cpuService;
+        this.reviewService = reviewService;
     }
 
     @Override
@@ -46,5 +52,53 @@ public class LoadInitialData implements CommandLineRunner {
         initialAmdCpu2.setFrequency("3.2 GHz");
         initialAmdCpu2.setCoreCount(6);
         cpuService.uploadCPU(initialAmdCpu2);
+
+        List<CPUModelUITO> cpus = cpuService.getAllCPUModell();
+
+        for (CPUModelUITO cpu : cpus) {
+            if (cpu.getId() == 1) {
+                ReviewOfCPUUITO review1 = new ReviewOfCPUUITO();
+                review1.setReviewText("Remek processzor, ajánlanám!");
+                review1.setScore(5);
+                review1.setRecommend(true);
+                review1.setCpuModelUITO(cpu);
+                reviewService.uploadReview(cpu.getId(), review1);
+
+                ReviewOfCPUUITO review2 = new ReviewOfCPUUITO();
+                review2.setReviewText("Nem rossz, de ennyi pénzért lehetne jobb is.");
+                review2.setScore(3);
+                review2.setRecommend(false);
+                review2.setCpuModelUITO(cpu);
+                reviewService.uploadReview(cpu.getId(), review2);
+            } else if (cpu.getId() == 2) {
+                ReviewOfCPUUITO review3 = new ReviewOfCPUUITO();
+                review3.setReviewText("Kicsit drága volt, de a célnak megfelel!");
+                review3.setScore(4);
+                review3.setRecommend(true);
+                review3.setCpuModelUITO(cpu);
+                reviewService.uploadReview(cpu.getId(), review3);
+
+                ReviewOfCPUUITO review4 = new ReviewOfCPUUITO();
+                review4.setReviewText("Nem ajánlanám ennyi pénzért, van jobb is.");
+                review4.setScore(2);
+                review4.setRecommend(false);
+                review4.setCpuModelUITO(cpu);
+                reviewService.uploadReview(cpu.getId(), review4);
+            } else {
+                ReviewOfCPUUITO review3 = new ReviewOfCPUUITO();
+                review3.setReviewText("Totál jó processzor, gratulálok a tervezőknek!");
+                review3.setScore(5);
+                review3.setRecommend(true);
+                review3.setCpuModelUITO(cpu);
+                reviewService.uploadReview(cpu.getId(), review3);
+
+                ReviewOfCPUUITO review4 = new ReviewOfCPUUITO();
+                review4.setReviewText("Remek, minden szempontból tökéletes választás!");
+                review4.setScore(5);
+                review4.setRecommend(false);
+                review4.setCpuModelUITO(cpu);
+                reviewService.uploadReview(cpu.getId(), review4);
+            }
+        }
     }
 }
